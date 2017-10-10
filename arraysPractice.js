2a6c4da526f13805e6ec20685f085b3fd627de42
Mario A. Hoyos <mariohoyos92@gmail.com>
2017-10-10 17:49:16 -0500

    finished all of the extra credit and all tests passed!

--- arraysPractice.js
+++ arraysPractice.js
@@ -138,7 +138,42 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
-  //Code Here
+  const removeItem = (myGroceryList, item) =>{
+    if(Array.isArray(myGroceryList)){
+      if(typeof item == "string"){
+        for(let i = 0; i < myGroceryList.length; i++){
+          if(myGroceryList[i] === item){
+            myGroceryList.splice(i,1);
+            return myGroceryList;
+          } 
+            
+          
+        }return myGroceryList;
+
+      }else{
+        return [];
+      }
+
+    }else {
+      return [];
+    }
+
+
+  }
+
+  const addItem = (myGroceryList, item) => {
+    if(Array.isArray(myGroceryList)){
+      if(typeof item === "string"){
+        myGroceryList.push(item);
+        return myGroceryList;
+      } else {
+        return [];
+      }
+
+    } else {
+      return [];
+    }
+  }
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -151,7 +186,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+  const maker = () => {
+    array = [];
+    for(let i = 1; i <= 215; i++){
+      array.push(i);
+    }
+    return array;
+  }
 
 
 
@@ -162,7 +203,19 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
-  //Code Here
+function addTen( arr ) {
+  var newArr = [];
+  
+  for( var i = 0; i < arr.length; i++ ) {
+    if((typeof arr[i]) === "number"){
+      newArr.push(arr[i] + 10);
+    }else{
+      newArr.push(Number(arr[i]) + 10)
+    }
+  }
+  
+  return newArr;
+}
 
 
 
@@ -183,7 +236,7 @@ for(var i = 0; i < num2; i++){
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+  const longer = (arr1,arr2) => arr1.length > arr2.length ? arr1 : arr2;
 
 
 /*
@@ -196,7 +249,17 @@ Your 'both' function will be given two arguments, arr1 and arr2 (from the previo
 Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-  //Code Here
+  const both = (arr1,arr2) => {
+    let newArray =[];
+    for(let i = 0; i < arr1.length; i++){
+      
+        if(arr1.indexOf(arr2[i]) !== -1){
+          newArray.push(arr2[i]);
+        }
+      
+    }
+    return newArray;
+  }
 
 
 
@@ -236,12 +299,17 @@ var colt = {
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+  devMountainEmployees.push(tyler,cahlan,ryan,colt);
+  console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+  for(let i = 0; i <devMountainEmployees.length; i++){
+    if(devMountainEmployees[i].name === "Cahlan"){
+      devMountainEmployees.splice(i,1);
+    }
+  }
 
 
 
@@ -254,7 +322,7 @@ of Data is to have an Array full of objects. */
 
 //Create an empty array called users.
 
-  //Code Here
+  users = [];
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
@@ -267,7 +335,24 @@ var user1 = {
     username: 'infiniateLoop'
 };
 
-//Your Code Here
+users = [{
+  name: 'Tyler McGinnis',
+  email: 'tylermcginnis33@gmail.com',
+  password: 'iLoveJavaScript',
+  username: 'infiniateLoop'
+  },
+  {
+name: 'Mario',
+email: 'mariomcginnis33@gmail.com',
+password: 'me',
+username: 'dope'
+  },
+  {
+name: 'bob McGinnis',
+email: 'bobmcginnis33@gmail.com',
+password: 'iLoveyouvaScript',
+username: 'infiniatasdfeLoop'
+  }];
 
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
@@ -277,6 +362,10 @@ and those objects contain properties about the specific person you follow.*/
 objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 Once you find the particular index he's located in, delete him from the array.*/
 
-  //Code Here
+  for(let i = 0; i < users.length; i++){
+    if(users[i].email === "tylermcginnis33@gmail.com"){
+      users.splice(i,1);
+    }
+  }
 
 //The activity we just did is very much how data works in 'the real world'.
